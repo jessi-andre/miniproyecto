2c68706c77b11d1fa37ad4f7b5a710e8916d3191
jessi-andre <jessicadaianaandre@gmail.com>
2021-09-21 11:15:12 -0300

    Subiendo cambios

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express')
- const app = express() 
+const express = require('express') /*llamando a express*/
+const path = require('path');
 
+const app = express();
+app.use(express.static('public'));
 
 const port =3002;
 
-app.get('/',(req, res) => res.send('¡¡Hola mundo!!'))
-
  app.listen(port, () => console.log("Servidor corriendo en el puerto " + port));
+ /*req(pedido), res(respuesta)*/
+ app.get('/', (req, res) => {
+res.sendFile( path.join(__dirname, './views/index.html'));
+    })  
 
-console.log(1); 
